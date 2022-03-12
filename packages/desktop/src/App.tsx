19cad8fa343f09e69fbbea7179942c35c1a4435d
talksik <patel.arjun50@gmail.com>
2022-03-12 14:57:49 -0500

    simple login page

--- packages/desktop/src/App.tsx
+++ packages/desktop/src/App.tsx
@@ -1,28 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
-import logo from './logo.svg';
-import testConnection from '@nirvana/core';
+import { Button } from "@mui/material";
+import Logo from "./components/Logo";
+import React from "react";
+import logo from "./logo.svg";
+import testConnection from "@nirvana/core";
 
-testConnection()
+testConnection();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className=" text-lg">
-          Edit <code>src/App.tsx</code> awoahhh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container flex flex-col space-y-5 mx-auto my-auto justify-center h-screen">
+      <Logo />
+      <Button variant="contained">Sign In</Button>
     </div>
   );
 }
