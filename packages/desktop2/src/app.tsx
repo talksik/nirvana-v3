20ddc5a324328f460a87aeadc3c58378138b07f6
talksik <patel.arjun50@gmail.com>
2022-03-17 09:55:11 -0400

    adding desktop 2 app with electron forge

---
+++ packages/desktop2/src/app.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function render() {
+  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById("root"));
+}
+
+render();
