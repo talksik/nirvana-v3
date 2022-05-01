9e377c9ba8595dd086f0524edc6229e77df6eac0
talksik <patel.arjun50@gmail.com>
2022-05-01 11:20:04 -0500

    Update index.tsx

--- packages/desktop/src/pages/terminal/index.tsx
+++ packages/desktop/src/pages/terminal/index.tsx
@@ -3,6 +3,7 @@ import { Button, Fab, InputAdornment, TextField } from "@mui/material";
 import { FaPlus, FaSearch } from "react-icons/fa";
 import { useRef, useState } from "react";
 
+import CellTowerSharpIcon from "@mui/icons-material/CellTowerSharp";
 import IconButton from "../../components/Button/IconButton/index";
 import NirvanaHeader from "../../components/header";
 
@@ -11,10 +12,21 @@ export default function NirvanaTerminal() {
     <div className="flex flex-col flex-1">
       <NirvanaHeader />
 
-      <div className="flex flex-row items-stretch m-2 gap-2">
-        <IconButton>
-          <FaPlus />
-        </IconButton>
+      {/* main terminal content */}
+      <div className="m-2">
+        {/* header */}
+        <div className="flex flex-row items-center">
+          <span className="flex flex-row gap-2 justify-start text-slate-800">
+            <CellTowerSharpIcon />
+
+            <h2 className="text-inherit font-semibold ">Tuned In</h2>
+
+            <p className="text-slate-300 text-xs">3/5</p>
+          </span>
+          <IconButton>
+            <FaPlus />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
