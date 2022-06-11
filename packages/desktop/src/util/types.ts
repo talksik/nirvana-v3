9504846172612b66741feeae89b093370e7eb313
talksik <patel.arjun50@gmail.com>
2022-06-11 11:10:45 -0500

    quick tip for sorting with temporary top in inbox

--- packages/desktop/src/util/types.ts
+++ packages/desktop/src/util/types.ts
@@ -3,7 +3,14 @@ import Conversation from '@nirvana/core/models/conversation.model';
 import User from '@nirvana/core/models/user.model';
 
 export type ConversationMap = {
-  [conversationId: string]: Conversation & { tunedInUsers: string[]; connectedUserIds: string[] };
+  [conversationId: string]: Conversation & {
+    tunedInUsers: string[];
+    connectedUserIds: string[];
+
+    // allows client side to have this pushed up in the list and can uncheck it once user is done with this
+    // if action is take, normal ordering should take place with database upserts
+    temporaryInboxTop: boolean;
+  };
 };
 
 export type UserMap = {
