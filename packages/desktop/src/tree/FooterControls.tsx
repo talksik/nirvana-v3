89c9769525d654ed31570f31ea7ce60f0a5a63a1
talksik <patel.arjun50@gmail.com>
2022-06-20 11:56:25 -0500

    Update FooterControls.tsx

--- packages/desktop/src/tree/FooterControls.tsx
+++ packages/desktop/src/tree/FooterControls.tsx
@@ -328,9 +328,8 @@ function UserSettingsDialog({ open, handleClose }: { handleClose: () => void; op
                 ))}
               </Select>
               <FormHelperText>
-                Your speaker selection should be done on your computer status or taskbar. Please
-                select distinct speaker and microphone to avoid distortion when you have a bluetooth
-                device connected.
+                Please select distinct speaker and microphone to avoid distortion when you have a
+                bluetooth device connected.
               </FormHelperText>
             </FormControl>
 
